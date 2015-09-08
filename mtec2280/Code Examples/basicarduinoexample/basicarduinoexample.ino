043512707d2f4a810b8e595788ac81d974697503
zeven <zevenwolf@gmail.com>
2015-09-08 15:58:38 -0400

    basic arduino example

---
+++ mtec2280/Code Examples/basicarduinoexample/basicarduinoexample.ino
@@ -0,0 +1,22 @@
+/*
+Global Variables 
+
+int pinNumber = 2;
+
+*/
+
+/*
+This a block comment
+*/
+
+//Single comment
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
