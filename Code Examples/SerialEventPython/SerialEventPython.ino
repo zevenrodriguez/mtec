0f6c17be3b7d7683aec03a1b82b84191f89101ee
zeven <zevenwolf@gmail.com>
2015-12-02 11:02:42 -0500

    python changes

--- Code Examples/SerialEventPython/SerialEventPython.ino
+++ Code Examples/SerialEventPython/SerialEventPython.ino
@@ -60,7 +60,13 @@ void serialEvent() {
     // get the new byte:
     char inChar = (char)Serial.read();
     // add it to the inputString:
-    inputString += inChar;
+    //inputString += inChar;
+
+
+    if (inChar != '\n') {
+      inputString += inChar;
+    }
+
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
     if (inChar == '\n') {
