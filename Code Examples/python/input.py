86e450c5f38c18f7d5c2490799a6d68ddd7c1371
zeven <zevenwolf@gmail.com>
2016-02-09 12:31:23 -0500

    organization

--- Code Examples/python/input.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-while (True) :  # This constructs an infinite loop
-   command = input("Enter a command: ")
-   if command == "exit":
-       break
-   else:
-       print(command)
--- Code Examples/python/simpleSerial.py
+++
@@ -1,4 +0,0 @@
-import serial
-ser = serial.Serial(0)
-print(ser.name)
-ser.close()
